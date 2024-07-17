2acdc216ae72fdb713104f02165586406384fce7
Yasmeen mahilang <yasmeen2002mahilang@gmail.com>
2024-07-18 00:59:18 +0530

    repo

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [
-    react(),
-  ],
+  base: '/expense-tracker-vite-react-firebase',
+  plugins: [react()],
   build: {
     outDir: 'dist',
   },
