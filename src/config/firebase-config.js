ac5e37388bbdbb6788e34b2c7c728e124baddb97
Yasmeen mahilang <yasmeen2002mahilang@gmail.com>
2024-07-17 20:38:20 +0530

    first commit

---
+++ src/config/firebase-config.js
@@ -0,0 +1,45 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAAXX0B3dR13RJvyjL9HpTNCUgvFo-PDaI",
+//   authDomain: "expense-tracker-app-87ecc.firebaseapp.com",
+//   projectId: "expense-tracker-app-87ecc",
+//   storageBucket: "expense-tracker-app-87ecc.appspot.com",
+//   messagingSenderId: "149287914651",
+//   appId: "1:149287914651:web:55aa9c0c0b8937e7a60a3e",
+//   measurementId: "G-2VXGV27HJR"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+
+
---
+++ src/pages/expense-tracker/index.jsx
@@ -0,0 +1,175 @@
+import { useState } from "react";
+import { signOut } from "firebase/auth";
+import { useAddTransaction } from "../../hooks/useAddTransaction";
+import { useGetTransactions } from "../../hooks/useGetTransactions";
+import { useGetUserInfo } from "../../hooks/useGetUserInfo";
+import { useNavigate } from "react-router-dom";
+
+import "./styles.css";
+import { auth } from "../../config/firebase-config";
+
+export const ExpenseTracker = () => {
+  const { addTransaction } = useAddTransaction();
+  const { transactions, transactionTotals } = useGetTransactions();
+  const { name, profilePhoto } = useGetUserInfo();
+  const navigate = useNavigate();
+
+  const [description, setDescription] = useState("");
+  const [transactionAmount, setTransactionAmount] = useState(0);
+  const [transactionType, setTransactionType] = useState("expense");
+
+  const { balance, income, expenses } = transactionTotals;
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addTransaction({
+      description,
+      transactionAmount,
+      transactionType,
+    });
+
+    setDescription("");
+    setTransactionAmount("");
+  };
+
+  const signUserOut = async () => {
+    try {
+      await signOut(auth);
+      localStorage.clear();
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <div className="expense-tracker">
+        <div className="container">
+          <h1> {name}'s Expense Tracker</h1>
+          <div className="balance">
+            <h3> Your Balance</h3>
+            {balance >= 0 ? <h2> Rs-{balance}/-</h2> : <h2> -Rs{balance * -1}</h2>}
+          </div>
+          <div className="summary">
+            <div className="income">
+              <h4> Income</h4>
+              <p>Rs-{income}</p>
+            </div>
+            <div className="expenses">
+              <h4> Expenses</h4>
+              <p>Rs-{expenses}</p>
+            </div>
+          </div>
+          <h2>ADD A NEW TRANSACTION</h2>
+          <form className="add-transaction" onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Description"
+              value={description}
+              required
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Amount"
+              value={transactionAmount}
+              required
+              onChange={(e) => setTransactionAmount(e.target.value)}
+            />
+            <input
+              type="radio"
+              id="expense"
+              value="expense"
+              checked={transactionType === "expense"}
+              onChange={(e) => setTransactionType(e.target.value)}
+            />
+            <label htmlFor="expense"> Expense</label>
+            <input
+              type="radio"
+              id="income"
+              value="income"
+              checked={transactionType === "income"}
+              onChange={(e) => setTransactionType(e.target.value)}
+            />
+            <label htmlFor="income"> Income</label>
+
+            <button type="submit"> Add Transaction</button>
+          </form>
+        </div>
+        {profilePhoto && (
+          <div className="profile">
+            {" "}
+            <img className="profile-photo" src={profilePhoto} alt="" />
+            <button className="sign-out-button" onClick={signUserOut}>
+              Sign Out
+            </button>
+          </div>
+        )}
+      </div>
+
+
+      {/* <div className="transactions">
+        <h3> Transactions</h3>
+        <ul>
+          {transactions.map((transaction) => {
+            const { description, transactionAmount, transactionType } =
+              transaction;
+            return (
+              <li>
+                <h4> {description} </h4>
+                <p>
+                  ${transactionAmount} •{" "}
+                  <label
+                    style={{
+                      color: transactionType === "expense" ? "red" : "green",
+                    }}
+                  >
+                    {" "}
+                    {transactionType}{" "}
+                  </label>
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div> */}
+      <div className="transactions">
+  <h3>Transactions</h3>
+  <div class="container">
+    <table className="responsive-table">
+      <thead className="table-header">
+        <tr>
+          <th className="col col-1">Description</th>
+          <th className="col col-2">Amount</th>
+          <th className="col col-3">Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map((transaction, index) => (
+          <tr key={index} className="table-row">
+            <td className="col col-1" data-label="Description">
+              {transaction.description}
+            </td>
+            <td className="col col-2" data-label="Amount">
+              ${transaction.transactionAmount}
+            </td>
+            <td className="col col-3" data-label="Type">
+              <label
+                style={{
+                  color: transaction.transactionType === "expense" ? "red" : "green",
+                }}
+              >
+                {transaction.transactionType}
+              </label>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+</div>
+
+    </>
+  );
+};
