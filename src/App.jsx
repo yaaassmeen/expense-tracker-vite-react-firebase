9c2be210fb03693772cfd45f0314d43a60bc89e8
Yasmeen mahilang <yasmeen2002mahilang@gmail.com>
2024-07-18 11:46:59 +0530

    fall

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Auth />} />
           <Route path="/expense-tracker" element={<ExpenseTracker />} />
+          <Route path="*" element={<div>Not Found</div>} />
         </Routes>
       </Router>
     </div>
