b694425102c8b5fee8ed021bf58c640abcd1c9f2
Yasmeen mahilang <yasmeen2002mahilang@gmail.com>
2024-07-18 12:18:00 +0530

    base

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import { ExpenseTracker } from "./pages/expense-tracker/index";
 function App() {
   return (
     <div className="App">
-      <Router basename="/expense-tracker-vite-react-firebase">
+      {/* <Router basename="/expense-tracker-vite-react-firebase"> */}
+      <Router>
         <Routes>
           <Route path="/" exact element={<Auth />} />
           <Route path="/expense-tracker" element={<ExpenseTracker />} />
-          <Route path="*" element={<div>Not Found</div>} />
+          {/* <Route path="*" element={<div>Not Found</div>} /> */}
         </Routes>
       </Router>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/expense-tracker-vite-react-firebase',
+  // base: '/expense-tracker-vite-react-firebase',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
